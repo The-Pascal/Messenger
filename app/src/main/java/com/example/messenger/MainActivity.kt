ef471bf5b79a3b3a0b60a1898035716aa5e6abda
Duke-Stark <chaharchandresh@gmail.com>
2020-04-16 20:08:02 +0530

    Added Login Activity and connected Login page with registration page

--- app/src/main/java/com/example/messenger/MainActivity.kt
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.messenger
 
+import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,14 +18,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //used to underline text
+        val alreadyhaveaccount = findViewById<TextView>(R.id.already_account_register)
+        alreadyhaveaccount.setPaintFlags(alreadyhaveaccount.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
+
 
         val registerbtn = findViewById<Button>(R.id.register_button_register)
 
+
+
+        //registration button click
         registerbtn.setOnClickListener{
-            val email = email_editText_register.text.toString()
-            val password = password_editText_register.text.toString()
+            var email = email_editText_register.text.toString()
+            var password = password_editText_register.text.toString()
+            Log.e("register","Email: $email and password: $password")
         }
 
+        //already have an account click listener
+        already_account_register.setOnClickListener{
+            val myintent = Intent(this, loginpage::class.java)
+            startActivity(myintent)
+        }
 
     }
 }
