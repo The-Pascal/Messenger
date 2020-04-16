7b635e07b086dedccb11d135c440dc97b07fbc98
Duke-Stark <chaharchandresh@gmail.com>
2020-04-16 22:14:01 +0530

    Successfully added firebase authentication system for registration

--- app/src/main/java/com/example/messenger/MainActivity.kt
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
@@ -22,16 +23,12 @@ class MainActivity : AppCompatActivity() {
         val alreadyhaveaccount = findViewById<TextView>(R.id.already_account_register)
         alreadyhaveaccount.setPaintFlags(alreadyhaveaccount.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
 
-
         val registerbtn = findViewById<Button>(R.id.register_button_register)
-
-
 
         //registration button click
         registerbtn.setOnClickListener{
-            var email = email_editText_register.text.toString()
-            var password = password_editText_register.text.toString()
-            Log.e("register","Email: $email and password: $password")
+
+            register()
         }
 
         //already have an account click listener
@@ -39,6 +36,33 @@ class MainActivity : AppCompatActivity() {
             val myintent = Intent(this, loginpage::class.java)
             startActivity(myintent)
         }
+
+    }
+
+    fun register(){
+
+        val email = email_editText_register.text.toString()
+        val password = password_editText_register.text.toString()
+        Log.e("register","Email: $email and password: $password")
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this , "Enter all fields first ",Toast.LENGTH_SHORT).show()
+        }
+
+        var auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful){
+                    return@addOnCompleteListener
+                }
+                else{
+                    Toast.makeText(this,"Account successfully created with uid : ${it.result?.user?.uid} ",Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Error creating account",Toast.LENGTH_SHORT).show()
+            }
 
     }
 }
--- app/src/main/java/com/example/messenger/loginpage.kt
+++ app/src/main/java/com/example/messenger/loginpage.kt
@@ -1,12 +1,31 @@
 package com.example.messenger
 
+import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_loginpage.*
 
 class loginpage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginpage)
+
+        //used to underline text
+        val backToRegistration = findViewById<TextView>(R.id.back_to_registration_login)
+        backToRegistration.setPaintFlags(backToRegistration.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
+
+        login_button_login.setOnClickListener{
+            val username = username_editText_login.text.toString()
+            val password = password_editText_login.text.toString()
+
+        }
+
+        backToRegistration.setOnClickListener{
+            val intent = Intent(this , MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
